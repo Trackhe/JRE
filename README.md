9abfdc02c9acd13bc6414b6cc6b1a79e95d788ff
Michael Nehring <michael@trackhe.de>
2022-04-15 13:35:36 +0200

    Init Commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+Base Java Images:
+
+https://hub.docker.com/u/adoptopenjdk
+https://hub.docker.com/_/openjdk
+
+Volume /usr/src/app
---
+++ build-trackhe.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+VERSION=$(./jq_l -r .version package.json)
+NAME=$(./jq_l -r .name package.json)  
+BUILD=$(./jq_l -r .build package.json)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+VERSION=$(./jq_m -r .version package.json)
+NAME=$(./jq_m -r .name package.json)  
+BUILD=$(./jq_m -r .build package.json)
+#JAVA_ARGS=$(./jq_m -r .args package.json)
+fi
+
+docker build -t trackhe/jre:$VERSION-$NAME-19 -f Java19/Dockerfile . --build-arg BUILD=$BUILD --build-arg JVM-ARGS="$JAVA_ARGS"
+docker build -t trackhe/jre:$VERSION-$NAME-18 -f Java18/Dockerfile . --build-arg BUILD=$BUILD --build-arg JVM-ARGS="$JAVA_ARGS"
+docker build -t trackhe/jre:$VERSION-$NAME-17 -f Java17/Dockerfile . --build-arg BUILD=$BUILD --build-arg JVM-ARGS="$JAVA_ARGS"
+docker build -t trackhe/jre:$VERSION-$NAME-11 -f Java11/Dockerfile . --build-arg BUILD=$BUILD --build-arg JVM-ARGS="$JAVA_ARGS"
+docker build -t trackhe/jre:$VERSION-$NAME-8 -f Java8/Dockerfile . --build-arg BUILD=$BUILD --build-arg JVM_ARGS="$JAVA_ARGS"
+
